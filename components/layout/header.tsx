8f8ce6062b25bbc678e86dbe6e23aecb68228814
woodmost <santiago.woodmost@outlook.com>
2024-03-28 00:21:46 +0800

    feat: updated logo

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,3 +1,4 @@
+import { Outfit } from "next/font/google";
 import Link from "next/link";
 
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -8,6 +9,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { MobileSidebar } from "./mobile-sidebar";
 
+const outfit = Outfit({ subsets: ["latin"] });
 export default function Header() {
   return (
     <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
@@ -17,20 +19,23 @@ export default function Header() {
             <div className="flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
+                width="24"
+                height="24"
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
+                fill="none"
                 stroke="currentColor"
-                className="mr-2 h-6 w-6"
+                strokeWidth="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-box h6 h6 mr-2"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-                />
+                <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
+                <path d="m3.3 7 8.7 5 8.7-5" />
+                <path d="M12 22V12" />
               </svg>
-
-              <h2 className="text-xl font-semibold tracking-tight">
+              <h2
+                className={`${outfit.className} text-xl font-bold tracking-tight`}
+              >
                 Image Cropper
               </h2>
             </div>
